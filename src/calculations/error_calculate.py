968c455c681dab0a1ed869d1aef90377d837c9d8
marina <marinakovaleva0@gmail.com>
2023-06-23 18:41:47 +0100

    still working on error code

--- src/calculations/error_calculate.py
+++ src/calculations/error_calculate.py
@@ -36,20 +36,22 @@ class ErrorCalculator:
     def calculate_error(self, numerical_df: pd.DataFrame):
 
         if self.flame_type == "stagnation":
-            print(self.exp_df)
-            print(numerical_df)
-            # lookup to the same condition (T_in, P, U, T_in, phi, blend):
-            merged_df = pd.merge(self.exp_df, numerical_df, on=['T_in', 'P', 'U', 'T_in', 'phi', 'blend'], how = 'inner')
-            print(merged_df)
+            # define the species that are compared by checking which ones are in the exp file:
+            species = [x for x in self.exp_df.columns if 'Er' in x]
+            self.exp_df.rename(columns={'flame_speed': 'flame_speed_exp'}, inplace=True)
+            merged_df = pd.merge(self.exp_df, numerical_df, on=['T_in', 'P', 'U', 'T', 'phi', 'blend'], how = 'inner')
+            merged_df['error_val'] = ((merged_df['flame_speed'] - merged_df['flame_speed_exp'])/merged_df['flame_speed Er'])**2
+            N = len(merged_df)
+            N_fsd = N = len(species)
+            return (1 / N) * merged_df['error_val'].sum()
+
 
         elif self.flame_type == "freely_prop":
             self.exp_df.rename(columns={'flame_speed': 'flame_speed_exp'}, inplace=True)
             merged_df = pd.merge(self.exp_df, numerical_df, on=['T_in', 'P', 'T_in', 'phi', 'blend'], how = 'inner')
             merged_df['error_val'] = ((merged_df['flame_speed'] - merged_df['flame_speed_exp'])/merged_df['flame_speed Er'])**2
             N = len(merged_df)
-            print((1 / N) * merged_df['error_val'].sum())
             return (1 / N) * merged_df['error_val'].sum()
-
 
         else:
             raise Exception('cannot recognise flame type')
